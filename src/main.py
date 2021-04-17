8311906feb0275eda0cbc0f0701e3d8713bce046
Leo Tisljaric <tisljaricleo@gmail.com>
2021-04-17 18:33:44 +0200

    Added functions for feature extraction, data import. fixes #2

--- src/main.py
+++ src/main.py
@@ -1,1 +1,50 @@
+import os
+import pickle
+import numpy as np
+from stm_features.feature_extraction import FeatureExtraction
+from stm_features.visualization import plot_heatmap
 
+
+def open_pickle(path):
+    """
+    Opens pickle data from defined path.
+    :param path: Path to pickle file.
+    :return:
+    """
+    try:
+        with open(path, "rb") as handle:
+            data = pickle.load(handle)
+            return data
+    except Exception as e:
+        if hasattr(e, "message"):
+            print(e.message)
+            return None
+        else:
+            print(e)
+            return None
+
+
+# from pathlib import Path
+# aaa = Path(os.getcwd()).parent.absolute()
+
+path = os.getcwd()
+data_path = str(path) + r"/data/all_matrices.pkl"
+stms = open_pickle(data_path)
+
+for stm in stms:
+    ext = FeatureExtraction(stm["stm"])
+
+    x, y = ext.get_mass_center()
+    velicina = ext.get_p_area()
+    p_kocenja = ext.get_p_break()
+    p_acc = ext.get_p_acceleration()
+    p_con = ext.get_p_congestion()
+    p_unst = ext.get_p_unstable()
+    p_ff = ext.get_p_freeflow()
+    p_other = ext.get_p_other()
+
+    plot_heatmap(ext.stm_orig, "stm")
+    print()
+
+
+print()
---
+++ src/stm_features/feature_extraction.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+
+class FeatureExtraction(object):
+    def __init__(self, stm):
+        self.stm = stm
+        self.stm_orig = stm
+        self.total_sum = np.sum(stm)
+        self.filter_stm()
+
+    def filter_stm(self):
+        """Filters STM"""
+        max_val = 0.2 * np.max(self.stm)  # Remove 20% of maximal value.
+        self.stm = np.where(self.stm < max_val, 0, self.stm)
+        self.stm = self.stm / np.sum(self.stm)
+
+    def get_mass_center(self):
+        """Computes the center of mas of the STM
+
+        Returns:
+            (int, int): Center of mass (x, y)
+        """
+        # marginal distributions
+        dx = np.sum(self.stm, 1)
+        dy = np.sum(self.stm, 0)
+        # expected values
+        X, Y = self.stm.shape
+        cx = np.sum(dx * np.arange(X))
+        cy = np.sum(dy * np.arange(Y))
+        return int(cx), int(cy)
+
+    def get_p_area(self):
+        """Gets the percentage of area coverage
+        Counts all chells that have value larger then 0, then divides it
+        with total area (20x20=400)
+
+        Returns:
+            (float): Percentage of covered area (0-1)
+        """
+        data = self.stm[np.where(self.stm > 0)]
+        return round(len(data) / float(400), 2)
+
+    def get_p_break(self):
+        return round(np.sum(self.stm_orig[13:19, 0:6]) / self.total_sum, 2)
+
+    def get_p_acceleration(self):
+        return round(np.sum(self.stm_orig[0:6, 13:19]) / self.total_sum, 2)
+
+    def get_p_congestion(self):
+        return round(np.sum(self.stm_orig[0:6, 0:6]) / self.total_sum, 2)
+
+    def get_p_unstable(self):
+        return round(np.sum(self.stm_orig[7:12, 7:12]) / self.total_sum, 2)
+
+    def get_p_freeflow(self):
+        return round(np.sum(self.stm_orig[13:19, 13:19]) / self.total_sum, 2)
+
+    def get_p_other(self):
+        sumator = (
+            np.sum(self.stm_orig[7:12, 0:6])
+            + np.sum(self.stm_orig[0:6, 7:12])
+            + np.sum(self.stm_orig[13:19, 7:12])
+            + np.sum(self.stm_orig[7:12, 13:19])
+        )
+        return round(sumator / self.total_sum, 2)
