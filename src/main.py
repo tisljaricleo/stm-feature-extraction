ec6ce96db9456dc2b3d439dd43a4b13a36272bfa
Leo Tisljaric <tisljaricleo@gmail.com>
2021-04-23 19:33:42 +0200

    changed var names

--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
 import os
-from .stm_features import extract_features
-from .ml_algorithms import get_comparison
+from stm_features import extract_features
+from ml_algorithms import get_comparison
 
-# path = os.getcwd()
-# data_path = str(path) + r"/data/all_matrices.pkl"
-# save_path = str(path) + r"/data/final_dataframe.pkl"
-
-from pathlib import Path
-
-path = Path(os.getcwd()).parent.absolute()
+path = os.getcwd()
 data_path = str(path) + r"/data/all_matrices.pkl"
 save_path = str(path) + r"/data/final_dataframe.pkl"
+
+# from pathlib import Path
+
+# path = Path(os.getcwd()).parent.absolute()
+# data_path = str(path) + r"/data/all_matrices.pkl"
+# save_path = str(path) + r"/data/final_dataframe.pkl"
 
 print("Starting with feature extraction...")
 extract_features(data_path, save_path)
